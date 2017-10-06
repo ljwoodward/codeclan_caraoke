386e8622c60dc14666d055d5a89932e37d8cdab8
Laurence <ljwoodward.code@gmail.com>
2017-10-06 11:00:56 +0100

    Created Song class and tested attr_reader.

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -5,11 +5,15 @@ require_relative("../song.rb")
 class TestSong < MiniTest::Test
 
   def setup
-    @song1 = Song.new("Total Eclipse of the Heart")
+    @song1 = Song.new("Total Eclipse of the Heart", "Bonnie Tyler")
   end
 
   def test_get_title
     assert_equal(@song1.title, "Total Eclipse of the Heart")
+  end
+
+  def test_get_artist
+    assert_equal(@song1.artist, "Bonnie Tyler")
   end
 
 
