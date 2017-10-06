dcbc6046684c4bf58b1b4e6009c01a7f07dd842d
Laurence <ljwoodward.code@gmail.com>
2017-10-06 11:11:18 +0100

    created and tested Guest class

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -4,5 +4,13 @@ require_relative("../guest.rb")
 
 class TestGuest < MiniTest::Test
 
+  def setup()
+    @guest1 = Guest.new("Gunther")
+  end
+
+  def test_get_guest_name()
+    assert_equal(@guest1.name, "Gunther")
+  end
+
 
 end
